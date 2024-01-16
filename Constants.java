d0c3bd66e5a5caab7489840c6877b239922b436a
pratap2612 <156181303+pratap2612@users.noreply.github.com>
2024-01-16 21:28:22 +0530

    added new java file

---
+++ Constants.java
@@ -0,0 +1,7 @@
+package com;
+
+public class Constants {
+    public static final int MAX_VALUE = 100;
+    public static final String APPLICATION_NAME = "MyApp";
+    public static final double PI_CONSTANT=3.142;
+}
